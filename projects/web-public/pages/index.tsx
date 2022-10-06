745945aad71a8b998ec9e24afc9db130f64810ab
Luiz Henrique <luizhjramos@outlook.com>
2022-10-05 22:46:24 -0300

    feat: apply monorepo utils module in the next project

--- projects/web-public/pages/index.tsx
+++ projects/web-public/pages/index.tsx
@@ -1,4 +1,10 @@
+import { sum } from "@monorepo/utils/math/sum";
 
 export default function Home() {
-  return <h1>Olá Mundo!</h1>
+  return (
+    <>
+      <h1>Monorepo Architecture</h1>
+      <span>Importing local module from our monorepo: {sum(2, 10)}</span>
+    </>
+  );
 }
